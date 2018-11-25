9600ccd8b8da5bf6910657b102fbd9d48071a5c1
LexiFramness <42749319+LexiFramness@users.noreply.github.com>
2018-11-25 13:10:42 -0600

    Sound Test

---
+++ Final Project/Final Project Pong/sketch.js
@@ -0,0 +1,34 @@
+//Alexis Framness
+// Final Project
+
+
+// create a variables for balls
+let ball = [];
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  mySound = loadSound('./assets/pong.mp3');
+}
+
+function setup() {
+    // createCanvas the fills window
+    createCanvas(windowWidth, windowHeight);
+    mySound.setVolume(0.1);
+    mySound.play();
+
+    // create a new ball object of class type "Ball"
+    ball = new Ball (random(windowWidth), random(windowHeight), 50,color(random(255), random(255), random(255)));
+
+    for (let i = 0; i < 33; i++) {
+        ball[i] = new Ball(random(windowWidth), random(windowHeight), 50,color(random(255), random(255), random(255))) ;
+    }
+}
+
+function draw() {
+    background(0);
+    // call the ball's methods
+    for( i = 0; i < 33; i++ ){
+    ball[i].frame();
+    }
+
+  }
